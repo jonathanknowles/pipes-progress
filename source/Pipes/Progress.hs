60a5f7096e4627ddc15ccec962e62c740e184d91
Jonathan Knowles <mail@jonathanknowles.net>
2015-11-12 17:02:39 +0800

    Working version of runMonitoredEffect function. It's a bit ugly, but it works.

--- source/Pipes/Progress.hs
+++ source/Pipes/Progress.hs
@@ -3,18 +3,18 @@
 {-# LANGUAGE ScopedTypeVariables        #-}
 {-# LANGUAGE ExistentialQuantification  #-}
 {-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE RecordWildCards            #-}
 
 module Pipes.Progress
     where
 
 import Control.Concurrent (threadDelay)
-import Control.Concurrent.Async (cancel, waitCatch)
-import Control.Concurrent.Async.Lifted (Async, async, wait, withAsync)
-import Control.Exception (throwIO)
+import Control.Concurrent.Async.Lifted (Async, async, cancel, wait, withAsync)
 import Control.Monad (forever, unless)
 import Control.Monad.Trans (MonadIO, liftIO)
 import Control.Monad.Trans.Control (MonadBaseControl)
 import Data.Maybe (fromMaybe)
+import Data.Monoid ((<>))
 import Data.Time.Clock (NominalDiffTime, UTCTime, addUTCTime, diffUTCTime, getCurrentTime)
 import Pipes (Consumer, Effect (..), Pipe, Producer, await, runEffect, yield, (>->))
 import Pipes.Buffer (Buffer)
@@ -63,6 +63,10 @@ pauseThreadUntil t = do
         LT -> threadDelay $ truncate $ diffUTCTime t now * 1000000
         _  -> return ()
 
+data Signal a m r = Signal
+    { signal        :: Producer a m r
+    , signalDefault :: a }
+
 data Monitor a m = Monitor
     { monitor       :: Consumer (Terminated a) m ()
     , monitorPeriod :: TimePeriod }
@@ -75,23 +79,23 @@ data Monitor a m = Monitor
 -- |   update      chunk                                           chunk    update
 -- |
 runMonitoredEffect :: (MonadBaseControl IO m, MonadIO m)
-    => TimePeriod
-    -> a
-    -> Producer a m r
-    -> Consumer a m ()
-    -> m r
-runMonitoredEffect period start source target = do
-    b0 <- B.create $ P.latest start
+    => Signal a m r -> Monitor a m -> m r
+runMonitoredEffect Signal {..} Monitor {..} = do
+    b0 <- B.create $ P.latest $ Just signalDefault
     b1 <- B.create $ P.bounded 1
-    let e0 = source >-> (B.write b0 >> forever await)
-    let e1 = B.read b0 >-> yieldPeriodically period >-> B.write b1
-    let e2 = B.read b1 >-> target
-    withAsync (runEffect e2) $ \a -> do
-        result <- withAsync (runEffect e1) $ const (runEffect e0)
-        liftIO $ do
-            atomically $
-                B.writeOnce b1 . fromMaybe start =<< B.readOnce b0
-            wait a
+    let e0 = signal >-> P.map Just >-> (B.write b0 >> forever await)
+    let e1 = B.read b0 >-> yieldPeriodically monitorPeriod >-> P.concat >-> P.map Value >-> B.write b1
+    let e2 = B.read b1 >-> monitor
+    withAsync (runEffect e2) $ \a2 -> do
+        result <- withAsync (runEffect e1) $ \a1 -> do
+            result <- runEffect e0
+            liftIO $ do
+                atomically $ B.writeOnce b1 . Value . fromMaybe signalDefault . (>>= id) =<< B.readOnce b0
+                atomically $ B.writeOnce b0 Nothing
+                atomically $ B.writeOnce b1 End
+                B.seal b1
+                wait a2
+            pure result
         pure result
 
 asyncWithGC :: IO a -> IO (Async a)
